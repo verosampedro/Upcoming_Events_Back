f1f00ee428cf7b507e50b900774a318638e239b3
Alicia González Castro <aliglezcastro@gmail.com>
2024-01-29 19:37:10 +0100

    fist back commit

---
+++ src/main/java/VeroSlaves/Upcoming_Events_Back/UpcomingEventsBackApplication.java
@@ -0,0 +1,13 @@
+package VeroSlaves.Upcoming_Events_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingEventsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingEventsBackApplication.class, args);
+	}
+
+}
