fc57c4ee67ffc48924f1e1382b016e4227078244
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-06 16:41:51 +0100

    insert roles datas

--- src/main/java/veroslaves/upcoming_events_back/users/UserRepository.java
+++ src/main/java/veroslaves/upcoming_events_back/users/UserRepository.java
@@ -1,5 +1,6 @@
 package veroslaves.upcoming_events_back.users;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,13 @@
 /* Users */
 INSERT INTO users (id_user, email, password) VALUES (default, 'admin@upcoming.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
 INSERT INTO users (id_user, email, password) VALUES (default, 'daisy@upcoming.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+INSERT INTO users (id_user, email, password) VALUES (default, 'donald@upcoming.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+
+/* Roles */
+INSERT INTO roles (id_rol, name) VALUES (default, 'ROLE_ADMIN');
+INSERT INTO roles (id_rol, name) VALUES (default, 'ROLE_USER');
+
+/* Roles_Users */
+INSERT INTO roles_users (user_id, role_id) VALUES (1, 1);
+INSERT INTO roles_users (user_id, role_id) VALUES (2, 2);
+INSERT INTO roles_users (user_id, role_id) VALUES (3, 2);
