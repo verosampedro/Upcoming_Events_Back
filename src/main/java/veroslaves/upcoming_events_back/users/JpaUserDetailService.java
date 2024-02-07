1ea0eb72af33b9cf61cee405c82f5d83e143cf3d
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-07 15:39:33 +0100

    added Security Filter chain to get method

---
+++ src/main/java/veroslaves/upcoming_events_back/users/JpaUserDetailService.java
@@ -0,0 +1,32 @@
+package veroslaves.upcoming_events_back.users;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import veroslaves.upcoming_events_back.exceptions.UserNotFoundException;
+
+@Service
+public class JpaUserDetailService implements UserDetailsService{
+
+    UserRepository repository;
+
+    public JpaUserDetailService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        return repository
+        
+            .findByEmail(email)
+            .map(SecurityUser::new)
+            .orElseThrow(()-> new UsernameNotFoundException("User not found" + email));
+
+    }
+
+    
+    
+}
