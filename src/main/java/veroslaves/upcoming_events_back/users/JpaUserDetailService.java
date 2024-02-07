dc3b0c02abc93bddcbd4726015f993ed3a7c6dee
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-07 15:42:02 +0100

    merged with feature/Security

--- src/main/java/veroslaves/upcoming_events_back/users/JpaUserDetailService.java
+++ src/main/java/veroslaves/upcoming_events_back/users/JpaUserDetailService.java
@@ -5,8 +5,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import veroslaves.upcoming_events_back.exceptions.UserNotFoundException;
-
 @Service
 public class JpaUserDetailService implements UserDetailsService{
 
