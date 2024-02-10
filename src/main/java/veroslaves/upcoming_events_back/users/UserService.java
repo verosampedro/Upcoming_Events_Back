a50ed5ea7ef02252bcfaa0a7fa21d87ae9258ad3
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-02-10 18:18:37 +0100

    event sign up added

--- src/main/java/veroslaves/upcoming_events_back/users/UserService.java
+++ src/main/java/veroslaves/upcoming_events_back/users/UserService.java
@@ -1,16 +1,23 @@
 package veroslaves.upcoming_events_back.users;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import veroslaves.upcoming_events_back.events.Event;
+import veroslaves.upcoming_events_back.events.EventNotFoundException;
+import veroslaves.upcoming_events_back.events.EventRepository;
 import veroslaves.upcoming_events_back.exceptions.UserNotFoundException;
 
 @Service
 public class UserService {
     UserRepository repository;
-
+    EventRepository eventRepository;
 
     public UserService(UserRepository repository) {
         this.repository = repository;
@@ -36,5 +43,22 @@ public class UserService {
         return newUser;
     }
 
+    public User addEventToUser(Long eventId) throws Exception {
+        
+        SecurityContext contextHolder = SecurityContextHolder.getContext();
+        Authentication auth = contextHolder.getAuthentication();
+
+        User updatingUser = repository.findByEmail(auth.getName()).orElseThrow(() -> new UserNotFoundException("User not found"));
+        Event newEvent = eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found"));
+
+        Set<Event> userEvents = updatingUser.getEvents();
+        userEvents.add(newEvent);
+        
+        updatingUser.setEvents(userEvents);
+
+        User updatedUser = repository.save(updatingUser);
+        
+        return updatedUser;
+    }
 
 }
