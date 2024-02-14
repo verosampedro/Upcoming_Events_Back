8639c69c92866737d44637e107ef0c11d8849091
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-14 15:19:44 +0100

    include optional findByCity and findByEvent in both repositories

--- src/main/java/veroslaves/upcoming_events_back/cities/CityRepository.java
+++ src/main/java/veroslaves/upcoming_events_back/cities/CityRepository.java
@@ -1,8 +1,11 @@
 package veroslaves.upcoming_events_back.cities;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+    public Optional<City> findByCity(String city_name);
 
     
 }
--- src/main/java/veroslaves/upcoming_events_back/events/EventRepository.java
+++ src/main/java/veroslaves/upcoming_events_back/events/EventRepository.java
@@ -1,9 +1,11 @@
 package veroslaves.upcoming_events_back.events;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event,Long>{
 
-    
-    
+    public Optional<Event>findByName(String event_tittle);
+
 }
