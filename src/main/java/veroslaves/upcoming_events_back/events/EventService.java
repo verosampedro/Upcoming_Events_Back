6544d5d7df48576cff5d85b2bc8d727d053706cd
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-14 15:30:42 +0100

    created save function in EventServices

--- src/main/java/veroslaves/upcoming_events_back/events/EventService.java
+++ src/main/java/veroslaves/upcoming_events_back/events/EventService.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import veroslaves.upcoming_events_back.cities.City;
+import veroslaves.upcoming_events_back.cities.CityNotFoundException;
+import veroslaves.upcoming_events_back.cities.CityRepository;
 import veroslaves.upcoming_events_back.interfaces.IGenericFullService;
 
 @Service
 public class EventService implements IGenericFullService <Event> {
     
     EventRepository eventRepository;
+    CityRepository cityRepository;
 
-    public EventService(EventRepository eventRepository) {
+    
+
+    public EventService(EventRepository eventRepository, CityRepository cityRepository) {
         this.eventRepository = eventRepository;
+        this.cityRepository = cityRepository;
     }
 
     public List<Event> getAll() {
@@ -21,7 +28,9 @@ public class EventService implements IGenericFullService <Event> {
     }
 
     public Event getById(Long id) throws Exception {
+        
         Event event = eventRepository.findById(id).orElseThrow(() -> new EventNotFoundException("Event not found"));
+
 
         return event;
     }
@@ -31,4 +40,13 @@ public class EventService implements IGenericFullService <Event> {
 
         return event;
     } */
+
+    public Event save(Event event) throws CityNotFoundException{
+        City city = cityRepository.findByCity(event.getCity()).orElseThrow(()-> new CityNotFoundException("City not found"));
+        cityRepository.save(city);
+        event.setCity(city);
+        eventRepository.save(event);
+        return event;
+    }
+
 }
