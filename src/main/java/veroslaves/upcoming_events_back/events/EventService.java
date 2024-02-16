b4d43fbee983c2cd4c5c8c66d7281ee63733d23b
Vero-Sampedro <verosampedro@icloud.com>
2024-02-16 14:17:06 +0100

    put call backend

--- src/main/java/veroslaves/upcoming_events_back/events/EventService.java
+++ src/main/java/veroslaves/upcoming_events_back/events/EventService.java
@@ -3,7 +3,6 @@ package veroslaves.upcoming_events_back.events;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 import veroslaves.upcoming_events_back.interfaces.IGenericFullService;
 
 @Service
@@ -31,4 +30,22 @@ public class EventService implements IGenericFullService <Event> {
 
         return event;
     } */
+
+    public Event update(Long id, Event event) throws Exception {
+        
+        Event updatingEvent = eventRepository.findById(id).orElseThrow(() -> new EventNotFoundException("Event not found"));
+        
+        updatingEvent.setId(event.getId());
+        updatingEvent.setEvent_title(event.getEvent_title());
+        updatingEvent.setStart_date(event.getStart_date());
+        updatingEvent.setFinish_date(event.getFinish_date());
+        updatingEvent.setEvent_image(event.getEvent_image());
+        updatingEvent.setMax_participants(event.getMax_participants());
+        updatingEvent.setDescription(event.getDescription());
+        updatingEvent.setCity(event.getCity());
+
+        Event updatedEvent = eventRepository.save(updatingEvent);
+        
+        return updatedEvent;
+    }
 }
