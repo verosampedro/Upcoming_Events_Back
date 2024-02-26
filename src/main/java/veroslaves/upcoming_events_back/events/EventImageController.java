a4e9772c3a81195afca433c0886299166b2c34de
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-26 17:17:51 +0100

    created controller to post images

--- src/main/java/veroslaves/upcoming_events_back/events/EventImageController.java
+++ src/main/java/veroslaves/upcoming_events_back/events/EventImageController.java
@@ -1,6 +1,8 @@
 package veroslaves.upcoming_events_back.events;
 
+import org.hibernate.mapping.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
