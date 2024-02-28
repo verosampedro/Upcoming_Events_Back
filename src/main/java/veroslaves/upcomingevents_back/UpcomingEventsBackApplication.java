beac7343b12e61bdd6f6ce5fa1b13103fd4d850c
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-28 16:14:27 +0100

    pull vero and mark work

---
+++ src/main/java/veroslaves/upcomingevents_back/UpcomingEventsBackApplication.java
@@ -0,0 +1,13 @@
+package veroslaves.upcomingevents_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingEventsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingEventsBackApplication.class, args);
+	}
+
+}
