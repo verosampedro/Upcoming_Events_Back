2e30452522dc5cbfbfafdd8ea1c7d6cee42b77a0
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-28 12:37:16 +0100

    created spring MVC-specific configuration for static resource handling

--- src/main/java/veroslaves/upcoming_events_back/events/EventImageController.java
+++ src/main/java/veroslaves/upcoming_events_back/events/EventImageController.java
@@ -1,8 +1,6 @@
 package veroslaves.upcoming_events_back.events;
 
-import org.hibernate.mapping.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
